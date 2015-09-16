9d98ad67951b9f4d051c5c81bcb6fd839d4cd89f
JS Morisset <jsm@surniaulula.com>
2015-09-16 10:26:35 -0400

    v3.9.2 change for tested up to 4.3.1

--- wpsso.php
+++ wpsso.php
@@ -8,7 +8,7 @@
  * License URI: http://www.gnu.org/licenses/gpl.txt
  * Description: Make sure social websites present your content correctly, no matter how your webpage is shared - from buttons, browser add-ons, or pasted URLs.
  * Requires At Least: 3.1
- * Tested Up To: 4.3
+ * Tested Up To: 4.3.1
  * Version: 3.9.2
  * 
  * Copyright 2012-2015 - Jean-Sebastien Morisset - http://surniaulula.com/
