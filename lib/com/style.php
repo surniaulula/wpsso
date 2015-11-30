9d09c74ca0184159fc3f50dda2761a0faf19dade
JS Morisset <jsm@surniaulula.com>
2015-11-30 12:15:24 -0800

    v3.16.0 minor update to translation and CSS

--- lib/com/style.php
+++ lib/com/style.php
@@ -144,8 +144,8 @@ if ( ! class_exists( 'SucomStyle' ) ) {
 		margin:0;
 		white-space:nowrap;
 		font-weight:bold;
-		background:#e0e0e0;
-		border-right:1px solid #ccc;
+		background:#fcfcfc;
+		border-right:1px solid #ddd;
 	}
 	.'.$lca.'-notice .notice-message {
 		display:table-cell;
