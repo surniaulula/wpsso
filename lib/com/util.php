8bf11f10ba23f77c45d37b130444ba15755129a9
JS Morisset <jsm@surniaulula.com>
2016-04-21 07:16:43 -0400

    v3.29.6-1

--- lib/com/util.php
+++ lib/com/util.php
@@ -681,6 +681,11 @@ if ( ! class_exists( 'SucomUtil' ) ) {
 			);
 		}
 
+		// deprecated 2016/04/21
+		public static function is_front_page( $use_post = false ) {
+			self::is_home_page( $use_post );
+		}
+
 		// returns true if using a static home page (with page or posts content)
 		public static function is_home_page( $use_post = false ) {
 			$ret = false;
