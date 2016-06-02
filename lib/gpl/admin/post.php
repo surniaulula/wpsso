a5b92e4a8d201949bdcc63a501268ccb4a97a730
JS Morisset <jsm@surniaulula.com>
2016-06-02 07:18:13 -0400

    v3.32.3-1

--- lib/gpl/admin/post.php
+++ lib/gpl/admin/post.php
@@ -91,10 +91,13 @@ if ( ! class_exists( 'WpssoGplAdminPost' ) ) {
 
 			$table_rows = $form->get_md_form_rows( $table_rows, $form_rows, $head, $mod, $auto_draft_msg );
 
-			if ( $this->p->cf['lca'] === 'wpsso' ) {
+			// wpsso json is available, but not active
+			if ( ! empty( $this->p->cf['plugin']['wpssojson'] ) &&
+				empty( $this->p->cf['plugin']['wpssojson']['version'] ) ) {
+
 				$info = $this->p->cf['plugin']['wpssojson'];
 				$table_rows[] = '<td colspan="2" align="center"><p class="pro-about-msg">'.
-					sprintf( __( 'Additional Schema markup options are available when the %s extension is active.', 'wpsso' ),
+					sprintf( __( 'Activate the %s extension for additional Schema markup features and options.', 'wpsso' ),
 						'<a href="'.$info['url']['download'].'" target="_blank">'.$info['name'].'</a>' ).'</p></td>';
 			}
 
