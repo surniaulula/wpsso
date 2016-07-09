b34028108c5f93489d1a570979859b214fcf1d88
JS Morisset <jsm@surniaulula.com>
2016-07-09 16:48:42 -0400

    v3.33.4-1

--- lib/submenu/essential.php
+++ lib/submenu/essential.php
@@ -161,18 +161,6 @@ if ( ! class_exists( 'WpssoSubmenuEssential' ) && class_exists( 'WpssoAdmin' ) )
 						'option label', 'wpsso' ), null, 'plugin_preserve' ).
 					'<td>'.$this->form->get_checkbox( 'plugin_preserve' ).'</td>';
 
-					$table_rows['plugin_debug'] = $this->form->get_th_html( _x( 'Add Hidden Debug Messages', 
-						'option label', 'wpsso' ), null, 'plugin_debug' ).
-					'<td>'.( SucomUtil::get_const( 'WPSSO_HTML_DEBUG' ) ? 
-						$this->form->get_no_checkbox( 'plugin_debug' ).' <em>WPSSO_HTML_DEBUG constant is true</em>' :
-						$this->form->get_checkbox( 'plugin_debug' ) ).'</td>';
-
-					if ( ! $this->p->check->aop( $this->p->cf['lca'], true, $this->p->is_avail['aop'] ) )
-						$table_rows['plugin_hide_pro'] = $this->form->get_th_html( _x( 'Hide All Pro Version Options',
-							'option label', 'wpsso' ), null, 'plugin_hide_pro' ).
-						'<td>'.$this->form->get_checkbox( 'plugin_hide_pro' ).'</td>';
-					else $this->form->get_hidden( 'plugin_hide_pro',  0, true );
-
 					break;
 			}
 			return $table_rows;
