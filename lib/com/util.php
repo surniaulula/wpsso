a62d2e679e4b0864b158518e4546e230d3a17dee
JS Morisset <jsm@surniaulula.com>
2016-07-23 10:51:52 -0400

    v3.33.7-dev4

--- lib/com/util.php
+++ lib/com/util.php
@@ -1492,6 +1492,25 @@ if ( ! class_exists( 'SucomUtil' ) ) {
 			$theme = wp_get_theme( $stylesheet, $theme_root );
 			return $theme->get_template().'-'.$theme->Version;
 		}
+
+		public static function get_all_image_sizes() {
+			global $_wp_additional_image_sizes; 
+			$sizes = array(); 
+			foreach ( get_intermediate_image_sizes() as $_size ) {
+				if ( in_array( $_size, array( 'thumbnail', 'medium', 'medium_large', 'large' ) ) ) {
+					$sizes[$_size]['width'] = get_option( $_size.'_size_w' );
+					$sizes[$_size]['height'] = get_option( $_size.'_size_h' );
+					$sizes[$_size]['crop'] = (bool) get_option( $_size.'_crop' );
+				} elseif ( isset( $_wp_additional_image_sizes[$_size] ) ) {
+					$sizes[$_size] = array(
+						'width' => $_wp_additional_image_sizes[$_size]['width'],
+						'height' => $_wp_additional_image_sizes[$_size]['height'],
+						'crop' => $_wp_additional_image_sizes[$_size]['crop'],
+					);
+				}
+			} 
+			return $sizes;
+		}
 	}
 }
 
--- lib/util.php
+++ lib/util.php
@@ -229,7 +229,7 @@ if ( ! class_exists( 'WpssoUtil' ) && class_exists( 'SucomUtil' ) ) {
 			}
 			if ( $this->p->debug->enabled ) {
 				$this->p->debug->mark( 'define image sizes' );	// end timer
-				$this->p->debug->log_arr( 'get_intermediate_image_sizes', get_intermediate_image_sizes() );
+				$this->p->debug->log_arr( 'get_all_image_sizes', SucomUtil::get_all_image_sizes() );
 			}
 		}
 
