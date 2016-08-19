ae25e8111145a721e44161b4dd279023bba05052
JS Morisset <jsm@surniaulula.com>
2016-08-19 07:16:32 -0300

    v3.34.1-dev2

--- lib/com/exception.php
+++ lib/com/exception.php
@@ -14,7 +14,50 @@ if ( ! class_exists( 'SucomException' ) ) {
 
 		protected $p;
 
-		public function __construct( $message = null, $code = 0, Exception $previous = null ) {
+		protected $statusCodes = array(
+			100 => 'Continue',
+			101 => 'Switching Protocols',
+			200 => 'OK',
+			201 => 'Created',
+			202 => 'Accepted',
+			203 => 'Non-Authoritative Information',
+			204 => 'No Content',
+			205 => 'Reset Content',
+			206 => 'Partial Content',
+			300 => 'Multiple Choices',
+			301 => 'Moved Permanently',
+			302 => 'Found',
+			303 => 'See Other',
+			304 => 'Not Modified',
+			305 => 'Use Proxy',
+			306 => '(Unused)',
+			307 => 'Temporary Redirect',
+			400 => 'Bad Request',
+			401 => 'Unauthorized',
+			402 => 'Payment Required',
+			403 => 'Forbidden',
+			404 => 'Not Found',
+			405 => 'Method Not Allowed',
+			406 => 'Not Acceptable',
+			407 => 'Proxy Authentication Required',
+			408 => 'Request Timeout',
+			409 => 'Conflict',
+			411 => 'Length Required',
+			412 => 'Precondition Failed',
+			413 => 'Request Entity Too Large',
+			414 => 'Request-URI Too Long',
+			415 => 'Unsupported Media Type',
+			416 => 'Requested Range Not Satisfiable',
+			417 => 'Expectation Failed',
+			500 => 'Internal Server Error',
+			501 => 'Not Implemented',
+			502 => 'Bad Gateway',
+			503 => 'Service Unavailable',
+			504 => 'Gateway Timeout',
+			505 => 'HTTP Version Not Supported'
+		);
+	
+		public function __construct( $message = null, $code = null, Exception $previous = null ) {
 
 			if ( class_exists( 'Wpsso' ) )
 				$this->p =& Wpsso::get_instance();
@@ -25,6 +68,10 @@ if ( ! class_exists( 'SucomException' ) ) {
 				if ( $this->p->debug->enabled )
 					$this->p->debug->mark();
 			}
+
+			if ( $message === null && isset( $this->statusCodes[(int) $code] ) )
+				$message = $this->statusCodes[(int) $code];
+
 			parent::__construct( $message, $code, $previous );
 		}
 
