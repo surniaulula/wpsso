9c43133ae3ebf2f60374adbf0435fc7fa4300f37
JS Morisset <jsm@surniaulula.com>
2016-10-31 17:35:48 -0700

    v3.37.0-b1

--- lib/config.php
+++ lib/config.php
@@ -21,7 +21,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			'setup_cache_exp' => 86400,	// 24 hours
 			'plugin' => array(
 				'wpsso' => array(
-					'version' => '3.37.0-dev4',	// plugin version
+					'version' => '3.37.0-b1',	// plugin version
 					'opt_version' => '469',		// increment when changing default options
 					'short' => 'WPSSO',		// short plugin name
 					'name' => 'WordPress Social Sharing Optimization (WPSSO)',
