9af0d1892620407b552cdd60074dcff1265442c7
JS Morisset <jsm@surniaulula.com>
2016-11-20 18:05:23 -0800

    v3.37.4-dev1

--- lib/config.php
+++ lib/config.php
@@ -340,7 +340,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 				'wpssotaq' => array(
 					'short' => 'WPSSO TAQ',		// short plugin name
 					'name' => 'WPSSO Tweet a Quote (WPSSO TAQ)',
-					'desc' => 'WPSSO extension to easily add Twitter-style quoted text with a Tweet share link in your post and page content.',
+					'desc' => 'WPSSO extension to easily add Twitter-style quoted text &mdash; with a Tweet share link &mdash; in your post and page content.',
 					'slug' => 'wpsso-tweet-a-quote',
 					'base' => 'wpsso-tweet-a-quote/wpsso-tweet-a-quote.php',
 					'update_auth' => '',
