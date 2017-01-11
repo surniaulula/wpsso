983375f6ca4ccd365b48003adec6da9e175b8435
JS Morisset <jsm@surniaulula.com>
2017-01-11 13:07:53 -0800

    v3.39.0-b2

--- wpsso.php
+++ wpsso.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Automatically create complete and accurate meta tags and Schema markup for Social Sharing Optimization (SSO) and SEO.
  * Requires At Least: 3.7
- * Tested Up To: 4.7
+ * Tested Up To: 4.7.1
  * Version: 3.39.0-b2
  * 
  * Version Numbering Scheme: {major}.{minor}.{bugfix}-{stage}{level}
