d02698ad906a21619cf2f7fe82f9658b17d7df97
JS Morisset <jsm@surniaulula.com>
2017-05-16 16:29:08 +0200

    v3.43.3

--- lib/config.php
+++ lib/config.php
@@ -511,7 +511,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 				'wpssoum' => array(
 					'short' => 'WPSSO UM',		// short plugin name
 					'name' => 'WPSSO Update Manager (WPSSO UM)',
-					'desc' => 'WPSSO extension to provide updates for the WPSSO Pro plugin and its Pro extensions.',
+					'desc' => 'WPSSO extension to provide updates for the WPSSO (WordPress Social Sharing Optimization) Pro plugin and its Pro extensions.',
 					'slug' => 'wpsso-um',
 					'base' => 'wpsso-um/wpsso-um.php',
 					'update_auth' => '',
