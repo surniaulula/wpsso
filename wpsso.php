575fde93e283ae26071e31d094055b1bbb5e365b
JS Morisset <jsm@surniaulula.com>
2017-06-01 06:49:26 +0200

    v3.44.3-dev.1

--- wpsso.php
+++ wpsso.php
@@ -307,14 +307,19 @@ if ( ! class_exists( 'Wpsso' ) ) {
 			 * Issue reminder notices and disable some caching when the plugin's debug mode is enabled.
 			 */
 			if ( $this->debug->enabled ) {
+				$info = $this->cf['plugin']['wpsso'];
 				if ( $this->debug->is_enabled( 'wp' ) ) {
 					$this->debug->log( 'WP debug log mode is active' );
 					$this->notice->warn( __( 'WP debug log mode is active &mdash; debug messages are being sent to the WordPress debug log.',
-						'wpsso' ).' '.__( 'Please disable debug mode when debugging operations are finished.', 'wpsso' ) );
+						'wpsso' ).' '.sprintf( __( 'Debug mode disables some %s caching features, which degrades performance slightly.',
+						'wpsso' ), $info['short'] ).' '.__( 'Please disable debug mode when debugging is complete.',
+						'wpsso' ) );
 				} elseif ( $this->debug->is_enabled( 'html' ) ) {
 					$this->debug->log( 'HTML debug mode is active' );
 					$this->notice->warn( __( 'HTML debug mode is active &mdash; debug messages are being added to webpages as hidden HTML comments.',
-						'wpsso' ).' '.__( 'Please disable debug mode when debugging operations are finished.', 'wpsso' ) );
+						'wpsso' ).' '.sprintf( __( 'Debug mode disables some %s caching features, which degrades performance slightly.',
+						'wpsso' ), $info['short'] ).' '.__( 'Please disable debug mode when debugging is complete.',
+						'wpsso' ) );
 				}
 				$this->util->add_plugin_filters( $this, array( 
 					'cache_expire_head_array' => '__return_zero',
