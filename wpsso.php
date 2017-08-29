82fd59a1fd266fdb38c2239f2bcb91d32fa1139d
JS Morisset <jsm@surniaulula.com>
2017-08-29 09:25:35 -0400

    v3.45.7-b.1

--- wpsso.php
+++ wpsso.php
@@ -13,6 +13,7 @@
  * Description: Automatically generate complete and accurate meta tags + Schema markup from your content for social media optimization (SMO) and SEO.
  * Requires At Least: 3.7
  * Tested Up To: 4.8.1
+ * Requires PHP: 5.3
  * Version: 3.45.7-b.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
