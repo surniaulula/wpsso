f5d15f99ea431f22de6612dea55e3134b4d28b64
JS Morisset <jsm@surniaulula.com>
2017-09-30 11:16:54 -0400

    v3.46.3-dev.1

---
+++ lib/com-gettext-extract.php
@@ -0,0 +1,37 @@
+<?php die( 'These aren\'t the droids you\'re looking for...' );
+// extract of translatable text strings from common library classes
+__( '%1$d important %2$s notice has been hidden and/or dismissed &mdash; <a id="%3$s">unhide and view the %2$s message</a>.', wpsso );
+__( '%1$d important %2$s notices have been hidden and/or dismissed &mdash; <a id="%3$s">unhide and view the %2$s messages</a>.', wpsso );
+__( 'All WordPress filter hooks must return their data / text and not send it to the output.', wpsso );
+__( '---BEGIN---', wpsso );
+__( 'Cache file %s is not readable.', wpsso );
+__( 'Cache folder %s is not writable.', wpsso );
+__( 'Dismiss', wpsso );
+__( '---END---', wpsso );
+__( 'Error connecting to %1$s for caching (HTTP code %2$d).', wpsso );
+__( 'Error removing cache file %s.', wpsso );
+__( 'Failed to create the %s cache folder.', wpsso );
+__( 'Failed to open the cache file %s for reading.', wpsso );
+__( 'Failed to open the cache file %s for writing.', wpsso );
+__( 'Incorrect data / text output:', wpsso );
+__( 'Notice', wpsso );
+__( 'PHP cURL library is missing &mdash; contact your hosting provider to have the cURL library installed.', wpsso );
+__( 'PHP "safe_mode" or "open_basedir" is defined &mdash; the PHP cURL library cannot follow URL redirects.', wpsso );
+__( 'Please contact the author of that filter hook and report this as a coding error / bug.', wpsso );
+__( 'Reference URL: %s', wpsso );
+__( 'Requests to cache this URL will be ignored for %d second(s).', wpsso );
+__( '%s Notice', wpsso );
+__( 'The "%1$s" filter hook with priority %2$d in the "%3$s" filter has mistakenly provided some output.', wpsso );
+__( 'The maximum number of URL redirects (%d) may have been exceeded.', wpsso );
+__( 'This notice can be dismissed temporarily for a period of %s.', wpsso );
+_x( 'checked', 'option value', wpsso );
+_x( 'default is %s', 'option value', wpsso );
+_x( '(default)', 'option value', wpsso );
+_x( 'error', 'notification type', wpsso );
+_x( '(no images)', 'option value', wpsso );
+_x( '[None]', 'option value', wpsso );
+_x( '(no videos)', 'option value', wpsso );
+_x( '(option disabled)', 'option value', wpsso );
+_x( 'unchecked', 'option value', wpsso );
+_x( 'warning', 'notification type', wpsso );
+?>
