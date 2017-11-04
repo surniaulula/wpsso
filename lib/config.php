9e7636c2efc1eb275c2b1dbb4b75baa5f8685688
JS Morisset <jsm@surniaulula.com>
2017-11-04 07:42:55 -0600

    v3.48.1-dev.1

--- lib/config.php
+++ lib/config.php
@@ -25,7 +25,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			),
 			'plugin' => array(
 				'wpsso' => array(
-					'version' => '3.48.0',		// plugin version
+					'version' => '3.48.1-dev.1',		// plugin version
 					'opt_version' => '560',		// increment when changing default options
 					'short' => 'WPSSO Core',	// short plugin name
 					'name' => 'WPSSO Core',
--- wpsso.php
+++ wpsso.php
@@ -14,8 +14,8 @@
  * Requires PHP: 5.3
  * Requires At Least: 3.7
  * Tested Up To: 4.8.3
- * WC Tested Up To: 3.2.2
- * Version: 3.48.0
+ * WC Tested Up To: 3.2.3
+ * Version: 3.48.1-dev.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
