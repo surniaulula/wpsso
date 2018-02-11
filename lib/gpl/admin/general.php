96f00668982066bcf37de9677e30a10ab15a544f
JS Morisset <jsm@surniaulula.com>
2018-02-11 13:39:44 -0600

    v3.53.0-dev.2

--- lib/gpl/admin/general.php
+++ lib/gpl/admin/general.php
@@ -27,68 +27,60 @@ if ( ! class_exists( 'WpssoGplAdminGeneral' ) ) {
 		}
 
 		public function filter_og_author_rows( $table_rows, $form ) {
-			if ( $this->p->debug->enabled )
+
+			if ( $this->p->debug->enabled ) {
 				$this->p->debug->mark();
+			}
 
-			$table_rows[] = '<td colspan="2" align="center">'.
-				$this->p->msgs->get( 'pro-feature-msg' ).'</td>';
+			$table_rows[] = '<td colspan="2" align="center">'.$this->p->msgs->get( 'pro-feature-msg' ).'</td>';
 
-			$table_rows[] = $form->get_th_html( _x( 'Include Author Gravatar Image',
-				'option label', 'wpsso' ), null, 'og_author_gravatar' ).
+			$table_rows['og_author_gravatar'] = ''.
+			$form->get_th_html( _x( 'Include Author Gravatar Image', 'option label', 'wpsso' ), null, 'og_author_gravatar' ).
 			'<td class="blank"><input type="checkbox" disabled="disabled" /></td>';
 
 			return $table_rows;
 		}
 
 		public function filter_og_videos_rows( $table_rows, $form ) {
-			if ( $this->p->debug->enabled )
+
+			if ( $this->p->debug->enabled ) {
 				$this->p->debug->mark();
+			}
 
 			$table_rows[] = '<td colspan="2" align="center">'.
 				'<p>'.__( 'Video discovery and integration modules are provided with the Pro version.',
-					'wpsso' ).'</p>'.
-				$this->p->msgs->get( 'pro-feature-msg' ).'</td>';
+					'wpsso' ).'</p>'.$this->p->msgs->get( 'pro-feature-msg' ).'</td>';
 
-			$table_rows['og_vid_max'] = '<tr class="hide_in_basic">'.
-			$form->get_th_html( _x( 'Maximum Videos to Include',
-				'option label', 'wpsso' ), null, 'og_vid_max' ).
+			$table_rows['og_vid_max'] = $form->get_tr_hide( 'basic', 'og_vid_max' ).
+			$form->get_th_html( _x( 'Maximum Videos to Include', 'option label', 'wpsso' ), null, 'og_vid_max' ).
 			'<td class="blank">'.$this->p->options['og_vid_max'].'</td>';
 
-			$table_rows['og_vid_https'] = '<tr class="hide_in_basic">'.
-			$form->get_th_html( _x( 'Use HTTPS for Video API Requests',
-				'option label', 'wpsso' ), null, 'og_vid_https' ).
-			'<td class="blank"><input type="checkbox" disabled="disabled" /> <em>'.
-				sprintf( _x( 'uses %s', 'option comment', 'wpsso' ),
-					str_replace( WPSSO_PLUGINDIR, WPSSO_PLUGINSLUG.'/', WPSSO_PHP_CURL_CAINFO ) ).'</em></td>';
+			$table_rows['og_vid_https'] = $form->get_tr_hide( 'basic', 'og_vid_https' ).
+			$form->get_th_html( _x( 'Use HTTPS for Video API Requests', 'option label', 'wpsso' ), null, 'og_vid_https' ).
+			'<td class="blank"><input type="checkbox" disabled="disabled" /> <em>'.sprintf( _x( 'uses %s', 'option comment', 'wpsso' ),
+				str_replace( WPSSO_PLUGINDIR, WPSSO_PLUGINSLUG.'/', WPSSO_PHP_CURL_CAINFO ) ).'</em></td>';
 
-			$table_rows['og_vid_prev_img'] = $form->get_th_html( _x( 'Include Video Preview Images',
-				'option label', 'wpsso' ), null, 'og_vid_prev_img' ).
-			'<td class="blank"><input type="checkbox" disabled="disabled" />'.' <em>'.
-				_x( 'video preview images are included first',
-					'option comment', 'wpsso' ).'</em></td>';
+			$table_rows['og_vid_prev_img'] = ''.
+			$form->get_th_html( _x( 'Include Video Preview Images', 'option label', 'wpsso' ), null, 'og_vid_prev_img' ).
+			'<td class="blank"><input type="checkbox" disabled="disabled" />  <em>'._x( 'video preview images are included first',
+				'option comment', 'wpsso' ).'</em></td>';
 
-			$table_rows['og_vid_html_type'] = $form->get_th_html( _x( 'Include text/html Type Meta Tags',
-				'option label', 'wpsso' ), null, 'og_vid_html_type' ).
+			$table_rows['og_vid_html_type'] = ''.
+			$form->get_th_html( _x( 'Include text/html Type Meta Tags', 'option label', 'wpsso' ), null, 'og_vid_html_type' ).
 			'<td class="blank"><input type="checkbox" disabled="disabled" /></td>';
 
-			$table_rows['og_vid_autoplay'] = $form->get_th_html( _x( 'Force Autoplay when Possible',
-				'option label', 'wpsso' ), null, 'og_vid_autoplay' ).
+			$table_rows['og_vid_autoplay'] = ''.
+			$form->get_th_html( _x( 'Force Autoplay when Possible', 'option label', 'wpsso' ), null, 'og_vid_autoplay' ).
 			'<td class="blank"><input type="checkbox" disabled="disabled" /></td>';
 
-			/**
-			 * Include option from the advanced settings page.
-			 */
-			$table_rows['plugin_embedded_media'] = '<tr class="hide_in_basic">'.
-			$form->get_th_html( _x( 'Check for Embedded Media', 'option label', 'wpsso' ), '', 'plugin_embedded_media' ).
-			'<td class="blank">'.
-			'<p>'.$form->get_nocb_cmt( 'plugin_facebook_api' ).' '._x( 'Facebook Videos', 'option value', 'wpsso' ).'</p>'.
-			'<p>'.$form->get_nocb_cmt( 'plugin_slideshare_api' ).' '._x( 'Slideshare Presentations', 'option value', 'wpsso' ).'</p>'.
-			'<p>'.$form->get_nocb_cmt( 'plugin_soundcloud_api' ).' '._x( 'Soundcloud Tracks', 'option value', 'wpsso' ).'</p>'.
-			'<p>'.$form->get_nocb_cmt( 'plugin_vimeo_api' ).' '._x( 'Vimeo Videos', 'option value', 'wpsso' ).'</p>'.
-			'<p>'.$form->get_nocb_cmt( 'plugin_wistia_api' ).' '._x( 'Wistia Videos', 'option value', 'wpsso' ).'</p>'.
-			'<p>'.$form->get_nocb_cmt( 'plugin_wpvideo_api' ).' '._x( 'WordPress Video Shortcode', 'option value', 'wpsso' ).'</p>'.
-			'<p>'.$form->get_nocb_cmt( 'plugin_youtube_api' ).' '._x( 'YouTube Videos and Playlists', 'option value', 'wpsso' ).'</p>'.
-			'</td>';
+			$check_embed_html = '';
+			foreach ( $this->p->cf['form']['embed_media_apis'] as $opt_key => $opt_label ) {
+				$check_embed_html .= '<p>'.$form->get_nocb_cmt( $opt_key ).' '._x( $opt_label, 'option value', 'wpsso' ).'</p>';
+			}
+
+			$table_rows['plugin_embed_media_apis'] = $form->get_tr_hide( 'basic', $this->p->cf['form']['embed_media_apis'] ).
+			$form->get_th_html( _x( 'Check for Embedded Media', 'option label', 'wpsso' ), '', 'plugin_embed_media_apis' ).
+			'<td class="blank">'.$check_embed_html.'</td>';
 
 			return $table_rows;
 		}
