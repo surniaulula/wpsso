91e940f97211f14d67e26586d0d3c4e701ae2064
JS Morisset <jsm@surniaulula.com>
2018-03-19 09:18:22 -0600

    v3.56.1

--- lib/messages.php
+++ lib/messages.php
@@ -1710,11 +1710,19 @@ if ( ! class_exists( 'WpssoMessages' ) ) {
 					case 'column-purchase-pro':
 
 						$text = '<p>'.sprintf( __( '<strong>%s includes:</strong>', 'wpsso' ), $info['short_pro'] ).'</p>';
+
 						$text .= '<ul>';
-						$text .= '<li>'.sprintf( __( '%s options for posts, pages, custom post types, terms (categories, tags, and custom taxonomies), and user profiles.', 'wpsso' ), _x( $this->p->cf['meta']['title'], 'metabox title', 'wpsso' ) ).'</li>';
-						$text .= '<li>'.__( 'Integration with numerous 3rd party plugins and external service APIs.', 'wpsso' ).'</li>';
-						$text .= '<li>'.__( 'Advanced plugin features and settings page.', 'wpsso' ).'</li>';
+
+						$text .= '<li>'.sprintf( __( '%s options for posts, pages, custom post types, terms (categories, tags, custom taxonomies), and user profiles.', 'wpsso' ), _x( $this->p->cf['meta']['title'], 'metabox title', 'wpsso' ) ).'</li>';
+
+						$text .= '<li>'.__( 'Integration with 3rd party plugins and service APIs.', 'wpsso' ).'</li>';
+
+						$text .= '<li>'.__( 'Advanced features and settings page.', 'wpsso' ).'</li>';
+
+						$text .= '<li>'.__( 'Ability to purchase Pro extensions.', 'wpsso' ).'</li>';
+
 						$text .= '</ul>';
+
 						$text .= '<p>'.__( '<strong>Nontransferable Pro licenses never expire</strong> &mdash; you may receive unlimited / lifetime updates and support for each licensed WordPress Site Address.', 'wpsso' ).' '.__( 'How great is that!?', 'wpsso' ).' :-)</p>';
 
 						if ( $this->p->avail['*']['p_dir'] ) {
@@ -1722,18 +1730,21 @@ if ( ! class_exists( 'WpssoMessages' ) ) {
 						} else {
 							$text .= '<p>'.sprintf( __( '<strong>Purchase %s easily and quickly with PayPal</strong> &mdash; update the Free plugin to Pro immediately after your purchase!', 'wpsso' ), $info['short_pro'] ).'</p>';
 						}
+
 						break;
 
 					case 'column-help-support':
 
 						$text = '<p>'.sprintf( __( '<strong>The continued development of %1$s is driven by user requests</strong> &mdash; we welcome all your comments and suggestions!', 'wpsso' ), $info['short'] ).'</p>';
+
 						break;
 
 					case 'column-rate-review':
 
 						$text = '<p>'.__( '<strong>Great reviews and ratings are a terrific way to encourage your plugin developers</strong> &mdash; and it only takes a minute.', 'wpsso' ).' ;-)</p>';
-						$text .= '<p>'.sprintf( __( 'Please encourage us %s by rating the plugins you use:',
-							'wpsso' ), '<span class="'.$lca.'-rate-heart"></span>' ).'</p>';
+
+						$text .= '<p>'.sprintf( __( 'Please encourage us %s by rating the plugins you use:', 'wpsso' ), '<span class="'.$lca.'-rate-heart"></span>' ).'</p>';
+
 						break;
 
 					default:
