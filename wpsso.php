9de0bdcf13adcd883849c555e038743603a0850a
JS Morisset <jsm@surniaulula.com>
2018-07-31 06:48:57 -0600

    v4.9.3

--- wpsso.php
+++ wpsso.php
@@ -15,7 +15,7 @@
  * Requires PHP: 5.4
  * Requires At Least: 3.8
  * Tested Up To: 4.9.7
- * WC Tested Up To: 3.4.3
+ * WC Tested Up To: 3.4.4
  * Version: 4.9.3
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
