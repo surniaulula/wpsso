60ab26e8ad2480d7088eed8cce51f94a7e45d5ea
JS Morisset <jsm@surniaulula.com>
2018-08-08 09:33:11 -0600

    v4.10.0-rc.1

--- lib/config.php
+++ lib/config.php
@@ -18,7 +18,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			'plugin' => array(
 				'wpsso' => array(			// Plugin acronym.
 					'version'     => '4.10.0-rc.1',	// Plugin version.
-					'opt_version' => '590',		// Increment when changing default option values.
+					'opt_version' => '591',		// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core [Main Plugin]',
 					'desc'        => 'WPSSO Core gives social sites and search engines better information about your content, business and authors, with complete meta tags and Schema markup for social sharing, Google Knowledge Graph / Rich Card SEO, Pinterest Rich Pins, Twitter Cards and more.',
