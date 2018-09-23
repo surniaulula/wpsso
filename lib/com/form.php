143c2610b54ed9b5decf960563fb04f153d48464
JS Morisset <jsm@surniaulula.com>
2018-09-23 09:15:39 -0600

    v4.13.1-b.6

--- lib/com/form.php
+++ lib/com/form.php
@@ -416,7 +416,7 @@ if ( ! class_exists( 'SucomForm' ) ) {
 
 											if ( css_class && css_class.indexOf( "hide_" ) == 0 ) {
 
-												this.data( "vc.accordion", function(){ return void 0; } );
+												this.data( "vc.accordion", this ).getRelatedTab = false;
 
 												this.trigger( ev );
 											}
