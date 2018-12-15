09afb96de2cb91f7d23589cb4be9cb2c5b8577de
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:39:16 -0600

    v4.18.0

---
+++ lib/gettext-com.php
@@ -0,0 +1,41 @@
+<?php die( 'These aren\'t the droids you\'re looking for...' );
+
+/**
+ * Extract of translatable text strings from the lib/com classes.
+ */
+__( '%s Notice', 'wpsso' );
+__( 'Notice', 'wpsso' );
+__( 'Error connecting to %1$s for caching (HTTP code %2$d).', 'wpsso' );
+__( 'The PHP "%s" setting is enabled &mdash; the PHP cURL library cannot follow URL redirects.', 'wpsso' );
+__( 'The "%s" setting is deprecated since PHP version 5.3 and removed from PHP since version 5.4.', 'wpsso' );
+__( 'Please contact your hosting provider to have this setting disabled or install a newer version of PHP.', 'wpsso' );
+__( 'The PHP "%s" setting is enabled &mdash; the PHP cURL library cannot follow URL redirects.', 'wpsso' );
+__( 'Please contact your hosting provider to have this setting disabled.', 'wpsso' );
+__( 'The maximum number of URL redirects (%d) may have been exceeded.', 'wpsso' );
+__( 'Requests to cache this URL will be ignored for %d second(s).', 'wpsso' );
+__( 'PHP cURL library is missing &mdash; contact your hosting provider to have the cURL library installed.', 'wpsso' );
+__( 'Error removing cache file %s.', 'wpsso' );
+__( 'Cache file %s is not readable.', 'wpsso' );
+__( 'Failed to open the cache file %s for reading.', 'wpsso' );
+__( 'Failed to create the %s cache folder.', 'wpsso' );
+__( 'Cache folder %s is not writable.', 'wpsso' );
+__( 'Failed to open the cache file %s for writing.', 'wpsso' );
+__( '%s Notice', 'wpsso' );
+__( 'Notice', 'wpsso' );
+__( 'Forever', 'wpsso' );
+__( 'This notice can be dismissed permanently.', 'wpsso' );
+__( 'This notice can be dismissed for %s.', 'wpsso' );
+__( 'edit', 'wpsso' );
+__( 'Reference: %s', 'wpsso' );
+// translators: please ignore - translation uses a different text domain
+__( 'The plugin information for "%s" contains an invalid download link.' );
+// translators: please ignore - translation uses a different text domain
+__( 'The plugin information for "%s" does not contain a download link.' );
+__( '%s Notice', 'wpsso' );
+__( 'Notice', 'wpsso' );
+__( 'The "%1$s" hook with priority %2$d in the "%3$s" filter has incorrectly sent output to the webpage.', 'wpsso' );
+__( 'All WordPress filter hooks must return their text, not send it to the webpage output.', 'wpsso' );
+__( 'Please contact the author of that filter hook and report this issue as a coding error.', 'wpsso' );
+__( 'Incorrect webpage output:', 'wpsso' );
+__( 'BEGIN OUTPUT', 'wpsso' );
+__( 'END OUTPUT', 'wpsso' );
