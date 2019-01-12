e20669c752b440e9f7c6a1b2a55d21780135a0cb
JS Morisset <jsm@surniaulula.com>
2019-01-12 11:27:51 -0600

    v4.19.0-dev.2

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			'lca'    => 'wpsso',	// Main plugin lowercase acronym (deprecated on 2017/11/18).
 			'plugin' => array(
 				'wpsso' => array(			// Plugin acronym.
-					'version'     => '4.18.5-dev.2',	// Plugin version.
+					'version'     => '4.19.0-dev.2',	// Plugin version.
 					'opt_version' => '623',		// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core [Main Plugin]',
