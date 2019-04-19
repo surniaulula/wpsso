9655ca74938a4444a766f5a595414404f49b9cf3
JS Morisset <jsm@surniaulula.com>
2019-04-19 10:47:13 -0700

    v4.29.0-rc.1

--- wpsso.php
+++ wpsso.php
@@ -13,8 +13,8 @@
  * Description: WPSSO Core makes sure your content looks great on all social and search sites - no matter how URLs are crawled, shared, re-shared, posted, or embedded!
  * Requires At Least: 3.8
  * Tested Up To: 5.1.1
- * WC Tested Up To: 3.5
- * Version: 4.29.0-b.1
+ * WC Tested Up To: 3.6
+ * Version: 4.29.0-rc.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
