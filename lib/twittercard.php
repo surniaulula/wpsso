9ead79625c04695a075117544a2f0125fa297f31
JS Morisset <jsm@surniaulula.com>
2019-05-29 05:29:01 -0700

    v4.31.2-dev.5

--- lib/twittercard.php
+++ lib/twittercard.php
@@ -72,7 +72,8 @@ if ( ! class_exists( 'WpssoTwitterCard' ) ) {
 			}
 
 			if ( ! isset( $mt_tc[ 'twitter:title' ] ) ) {
-				$mt_tc[ 'twitter:title' ] = $this->p->page->get_title( 70, '...', $mod, true, false, true, 'og_title' );
+				$mt_tc[ 'twitter:title' ] = $this->p->page->get_title( $max_len = 70, $dots = '...', $mod, $read_cache = true,
+					$add_hashtags = false, $do_encode = true, $md_key = 'og_title' );
 			}
 
 			if ( ! isset( $mt_tc[ 'twitter:description' ] ) ) {
@@ -81,8 +82,8 @@ if ( ! class_exists( 'WpssoTwitterCard' ) ) {
 					$this->p->debug->log( 'getting description for twitter:description meta tag' );
 				}
 
-				$mt_tc[ 'twitter:description' ] = $this->p->page->get_description( $this->p->options[ 'tc_desc_max_len' ], 
-					'...', $mod, $read_cache = true, $this->p->options[ 'og_desc_hashtags' ], $do_encode = true, $md_key = 'tc_desc' );
+				$mt_tc[ 'twitter:description' ] = $this->p->page->get_description( $this->p->options[ 'tc_desc_max_len' ], $dots = '...',
+					$mod, $read_cache = true, $this->p->options[ 'og_desc_hashtags' ], $do_encode = true, $md_key = 'tc_desc' );
 			}
 
 			if ( ! isset( $mt_tc[ 'twitter:creator' ] ) ) {
