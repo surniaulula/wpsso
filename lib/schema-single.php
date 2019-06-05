c638a93d87a60234d7ab253aa7d0bb69f1c30090
JS Morisset <jsm@surniaulula.com>
2019-06-05 06:01:22 -0700

    v4.31.3-dev.1

--- lib/schema-single.php
+++ lib/schema-single.php
@@ -501,6 +501,7 @@ if ( ! class_exists( 'WpssoSchemaSingle' ) ) {
 			$price_spec = WpssoSchema::get_data_itemprop_from_assoc( $mt_offer, array( 
 				'price'                 => 'product:price:amount',
 				'priceCurrency'         => 'product:price:currency',
+				'validFrom'             => 'product:sale_price_dates:start',
 				'validThrough'          => 'product:sale_price_dates:end',
 				'valueAddedTaxIncluded' => 'product:price:vat_included',
 			) );
