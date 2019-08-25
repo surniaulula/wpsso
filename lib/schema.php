add70007364a12f25b8140d3e9bd98918ee22cb7
JS Morisset <jsm@surniaulula.com>
2019-08-25 06:39:51 -0700

    v6.1.0-rc.2

--- lib/schema.php
+++ lib/schema.php
@@ -491,7 +491,7 @@ if ( ! class_exists( 'WpssoSchema' ) ) {
 						$this->p->debug->log( 'existing @type property is ' . print_r( $single_graph[ '@type' ], true ) );
 					}
 
-					$single_graph = apply_filters( $this->p->lca . '_json_single_graph_data',
+					$single_graph = apply_filters( $this->p->lca . '_json_data_graph_element',
 						$single_graph, $mod, $mt_og, $page_type_id, $is_main );
 
 					WpssoSchemaGraph::add_data( $single_graph );
