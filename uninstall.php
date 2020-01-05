6a4651e4b8cfe20975deaa14713b130b45bc6fcd
JS Morisset <jsm@surniaulula.com>
2020-01-05 12:33:46 -0800

    v6.16.2-rc.1

--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,7 @@
 /**
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
- * Copyright 2012-2019 Jean-Sebastien Morisset (https://wpsso.com/)
+ * Copyright 2012-2020 Jean-Sebastien Morisset (https://wpsso.com/)
  */
 
 if ( ! defined( 'ABSPATH' ) || ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
