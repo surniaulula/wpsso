13407d1fe896cee8ff5e56e89445d00927df6c5e
JS Morisset <jsm@surniaulula.com>
2020-02-21 07:59:27 -0800

    v6.21.2-rc.1

--- lib/schema.php
+++ lib/schema.php
@@ -193,7 +193,9 @@ if ( ! class_exists( 'WpssoSchema' ) ) {
 
 		 	/**
 			 * $org_id can be 'none', 'site', or a number (including 0).
+			 *
 		 	 * $logo_key can be 'org_logo_url' or 'org_banner_url' (600x60px image) for Articles.
+			 *
 			 * Do not provide localized option names - the method will fetch the localized values.
 			 */
 			if ( $this->p->debug->enabled ) {
