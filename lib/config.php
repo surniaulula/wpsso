cb9cb4b64526884f3033fa8cb63265ae5edff066
JS Morisset <jsm@surniaulula.com>
2020-03-17 10:21:00 -0700

    v6.25.0

--- lib/config.php
+++ lib/config.php
@@ -3770,6 +3770,13 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			return false;
 		}
 
+		/**
+		 * Load all essential library files.
+		 *
+		 * Avoid using is_admin() in this method as it can be unreliable this early in the load process. Some
+		 * plugins/themes do not define WP_ADMIN in their back-end templates (as they should), which is required to use
+		 * is_admin() early in the WordPress load process.
+		 */
 		public static function require_libs( $plugin_file_path ) {
 
 			require_once WPSSO_PLUGINDIR . 'lib/com/cache.php';
