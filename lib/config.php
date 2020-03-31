bdab39392337c2d5c2fe461c194fa5561205c7d7
JS Morisset <jsm@surniaulula.com>
2020-03-30 18:53:10 -0700

    v6.27.0-b.1

--- lib/config.php
+++ lib/config.php
@@ -395,7 +395,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 				'wpssojson' => array(			// Plugin acronym.
 					'short'       => 'WPSSO JSON',	// Short plugin name.
 					'name'        => 'WPSSO Schema JSON-LD Markup',
-					'desc'        => 'Google Rich Results with Structured Data for Articles, Carousels, Events, FAQ Pages, How-Tos, Images, Local Business, Products, Recipes, Reviews, Videos, and more.',
+					'desc'        => 'Provides Google Rich Results and Structured Data for Articles, Carousels (aka ItemList), Claim Reviews, Events, FAQ Pages, How-Tos, Images, Local Business / Local SEO, Organizations, Products, Ratings, Recipes, Restaurants, Reviews, Videos, and more.',
 					'slug'        => 'wpsso-schema-json-ld',
 					'base'        => 'wpsso-schema-json-ld/wpsso-schema-json-ld.php',
 					'update_auth' => 'tid',
