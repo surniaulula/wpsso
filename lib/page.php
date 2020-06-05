646e1a6d2dbf1945178c9c852c4110151e4931d8
JS Morisset <jsm@surniaulula.com>
2020-06-05 13:42:30 -0700

    v7.8.0

--- lib/page.php
+++ lib/page.php
@@ -894,6 +894,11 @@ if ( ! class_exists( 'WpssoPage' ) ) {
 
 				} else {
 
+					/**
+					 * The get_the_title() function does not apply the 'wp_title' filter.
+					 *
+					 * See https://core.trac.wordpress.org/browser/tags/5.4/src/wp-includes/post-template.php#L117.
+					 */
 					$title_text = html_entity_decode( get_the_title( $mod[ 'id' ] ) ) . ' ';
 
 					if ( $this->p->debug->enabled ) {
@@ -910,7 +915,9 @@ if ( ! class_exists( 'WpssoPage' ) ) {
 					$title_text .= $sep . ' ';
 				}
 
-				$title_text = $this->p->util->safe_apply_filters( array( 'wp_title', $title_text, $sep, 'right' ), $mod );
+				if ( $filter_title ) {
+					$title_text = $this->p->util->safe_apply_filters( array( 'wp_title', $title_text, $sep, 'right' ), $mod );
+				}
 
 			} elseif ( $mod[ 'is_term' ] ) {
 
@@ -929,7 +936,9 @@ if ( ! class_exists( 'WpssoPage' ) ) {
 
 				$title_text = $user_obj->display_name . ' ' . $sep . ' ';
 
-				$title_text = $this->p->util->safe_apply_filters( array( 'wp_title', $title_text, $sep, 'right' ), $mod );
+				if ( $filter_title ) {
+					$title_text = $this->p->util->safe_apply_filters( array( 'wp_title', $title_text, $sep, 'right' ), $mod );
+				}
 
 				$title_text = apply_filters( $this->p->lca . '_user_archive_title', $title_text, $mod, $user_obj );
 
@@ -941,7 +950,9 @@ if ( ! class_exists( 'WpssoPage' ) ) {
 					$this->p->debug->log( 'home posts get_site_name() = "' . $title_text . '"' );
 				}
 
-				$title_text = $this->p->util->safe_apply_filters( array( 'wp_title', $title_text, $sep, 'right' ), $mod );
+				if ( $filter_title ) {
+					$title_text = $this->p->util->safe_apply_filters( array( 'wp_title', $title_text, $sep, 'right' ), $mod );
+				}
 
 				$title_text = apply_filters( $this->p->lca . '_home_posts_title', $title_text, $mod );
 
@@ -972,11 +983,6 @@ if ( ! class_exists( 'WpssoPage' ) ) {
 			if ( ! $filter_title ) {
 
 				if ( $this->p->debug->enabled ) {
-
-					$this->p->debug->log( 'original wp_title value: ' . SucomUtil::get_original_filter_value( 'wp_title' ) );
-
-					$this->p->debug->log( 'modified wp_title value: ' . SucomUtil::get_modified_filter_value( 'wp_title' ) );
-
 					$this->p->debug->log( 'unprotecting filter value for wp_title' );
 				}
 
