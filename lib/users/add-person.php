86301a8c610adacf346c2b14ba862c8a0a519fac
JS Morisset <jsm@surniaulula.com>
2020-08-02 10:11:11 -0700

    v7.15.0-rc.1

--- lib/users/add-person.php
+++ lib/users/add-person.php
@@ -119,7 +119,7 @@ if ( ! class_exists( 'WpssoUsersAddPerson' ) && class_exists( 'WpssoAdmin' ) ) {
 
 			<div id="add-person-content">
 
-			<p><?php _e( 'Create a new person profile for use in Open Graph meta tags and Schema markup.', 'wpsso' ); ?></p>
+			<p><?php _e( 'Create a new person for use in Open Graph meta tags and Schema markup.', 'wpsso' ); ?></p>
 
 			<form method="post" name="createuser" id="createuser" class="validate" novalidate="novalidate">
 
