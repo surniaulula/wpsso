9f0568d2b9ca8627fda666edb54e223be1227542
JS Morisset <jsm@surniaulula.com>
2020-09-11 09:19:04 -0700

    v8.4.0

--- lib/admin.php
+++ lib/admin.php
@@ -3114,27 +3114,27 @@ if ( ! class_exists( 'WpssoAdmin' ) ) {
 		public function add_advanced_plugin_settings_table_rows( array &$table_rows, $form, $network = false ) {
 
 			$table_rows[ 'plugin_clean_on_uninstall' ] = '' .
-				$form->get_th_html( _x( 'Remove Settings on Uninstall', 'option label', 'wpsso' ), '', 'plugin_clean_on_uninstall' ) . 
-				'<td>' . $form->get_checkbox( 'plugin_clean_on_uninstall' ) . ' ' .
-				_x( 'including any custom meta for posts, terms and users', 'option comment', 'wpsso' ) . '</td>' .
-				self::get_option_site_use( 'plugin_clean_on_uninstall', $form, $network, true );
+			$form->get_th_html( _x( 'Remove Settings on Uninstall', 'option label', 'wpsso' ), '', 'plugin_clean_on_uninstall' ) . 
+			'<td>' . $form->get_checkbox( 'plugin_clean_on_uninstall' ) . ' ' .
+			_x( 'including any custom meta for posts, terms and users', 'option comment', 'wpsso' ) . '</td>' .
+			self::get_option_site_use( 'plugin_clean_on_uninstall', $form, $network, true );
 
 			$table_rows[ 'plugin_debug' ] = '' .
-				$form->get_th_html( _x( 'Add Hidden Debug Messages', 'option label', 'wpsso' ), '', 'plugin_debug' ) . 
-				'<td>' . ( ! $network && SucomUtil::get_const( 'WPSSO_HTML_DEBUG' ) ?
-					$form->get_no_checkbox( 'plugin_debug' ) . ' <em>' . _x( 'WPSSO_HTML_DEBUG constant is true', 'option comment', 'wpsso' ) . '</em>' :
-					$form->get_checkbox( 'plugin_debug' ) ) . '</td>' .
-				self::get_option_site_use( 'plugin_debug', $form, $network, true );
+			$form->get_th_html( _x( 'Add Hidden Debug Messages', 'option label', 'wpsso' ), '', 'plugin_debug' ) . 
+			'<td>' . ( ! $network && SucomUtil::get_const( 'WPSSO_HTML_DEBUG' ) ?
+				$form->get_no_checkbox( 'plugin_debug' ) . ' <em>' . _x( 'WPSSO_HTML_DEBUG constant is true', 'option comment', 'wpsso' ) . '</em>' :
+				$form->get_checkbox( 'plugin_debug' ) ) . '</td>' .
+			self::get_option_site_use( 'plugin_debug', $form, $network, true );
 
 			$table_rows[ 'plugin_show_opts' ] = '' .
-				$form->get_th_html( _x( 'Options to Show by Default', 'option label', 'wpsso' ), '', 'plugin_show_opts' ) .
-				'<td>' . $form->get_select( 'plugin_show_opts', $this->p->cf[ 'form' ][ 'show_options' ] ) . '</td>' .
-				self::get_option_site_use( 'plugin_show_opts', $form, $network, true );
+			$form->get_th_html( _x( 'Options to Show by Default', 'option label', 'wpsso' ), '', 'plugin_show_opts' ) .
+			'<td>' . $form->get_select( 'plugin_show_opts', $this->p->cf[ 'form' ][ 'show_options' ] ) . '</td>' .
+			self::get_option_site_use( 'plugin_show_opts', $form, $network, true );
 
 			$table_rows[ 'plugin_notice_system' ] = $form->get_tr_hide( 'basic', 'plugin_notice_system' ) .
-				$form->get_th_html( _x( 'Notification System', 'option label', 'wpsso' ), '', 'plugin_notice_system' ) .
-				'<td>' . $form->get_select( 'plugin_notice_system', $this->p->cf[ 'form' ][ 'notice_systems' ] ) . '</td>' .
-				self::get_option_site_use( 'plugin_notice_system', $form, $network, true );
+			$form->get_th_html( _x( 'Notification System', 'option label', 'wpsso' ), '', 'plugin_notice_system' ) .
+			'<td>' . $form->get_select( 'plugin_notice_system', $this->p->cf[ 'form' ][ 'notice_systems' ] ) . '</td>' .
+			self::get_option_site_use( 'plugin_notice_system', $form, $network, true );
 		}
 
 		/**
