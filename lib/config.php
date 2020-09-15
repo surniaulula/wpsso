42764bfc6a08e14b4bcf68a7d05685a50561e9da
JS Morisset <jsm@surniaulula.com>
2020-09-15 13:19:38 -0700

    v8.5.0

--- lib/config.php
+++ lib/config.php
@@ -22,7 +22,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			),
 			'plugin' => array(
 				'wpsso' => array(			// Plugin acronym.
-					'version'     => '8.5.0-rc.1',	// Plugin version.
+					'version'     => '8.5.0',	// Plugin version.
 					'opt_version' => '761',		// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core',
