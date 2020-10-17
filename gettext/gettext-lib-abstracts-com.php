99d5403fab14e8a8b69544a98d1513f134258c6b
JS Morisset <jsm@surniaulula.com>
2020-10-17 11:16:52 -0700

    v8.8.1

--- gettext/gettext-lib-abstracts-com.php
+++
@@ -1,7 +0,0 @@
-<?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable strings from lib/abstracts/com/.
- */
-__( 'The %1$s add-on requires the %2$s plugin &mdash; please activate the missing plugin.', 'wpsso' );
-__( 'The %1$s add-on requires %2$s version %3$s or newer (version %4$s is currently installed).', 'wpsso' );
