e02dc73c6e665f21ecf955e6b19d4201e98e6ba3
JS Morisset <jsm@surniaulula.com>
2020-11-09 09:28:27 -0800

    v8.11.2

--- lib/admin-head.php
+++ lib/admin-head.php
@@ -378,7 +378,7 @@ if ( ! class_exists( 'WpssoAdminHead' ) ) {
 
 				$notice_msg = sprintf( __( 'The WooCommerce v%s plugin is known to offer incomplete Schema markup for Google.', 'wpsso' ), $wc_version ) . ' ';
 
-				$notice_msg .= __( 'The WPSSO Core Premium plugin (required for WooCommerce integration) and its WPSSO Schema JSON-LD Markup add-on provide a much better solution by offering complete product meta tags for Facebook / Pinterest, and complete Schema product markup for Google Rich Results &mdash; including additional product images, product variations, product information (brand, color, condition, EAN, dimensions, GTIN-8/12/13/14, ISBN, material, MPN, size, SKU, weight, etc), product reviews, product ratings, sale start / end dates, sale prices, pre-tax prices, VAT prices, and much, much more.', 'wpsso' );
+				$notice_msg .= __( 'The WPSSO Core Premium plugin (required for WooCommerce integration) and its WPSSO Schema JSON-LD Markup add-on provide a much better solution by offering complete product meta tags for Facebook / Pinterest, and complete Schema product markup for Google Rich Results &mdash; including additional product images, product variations, product information (brand, color, condition, EAN, dimensions, GTIN-8/12/13/14, ISBN, material, MPN, size, SKU, volume, weight, etc), product reviews, product ratings, sale start / end dates, sale prices, pre-tax prices, VAT prices, shipping rates, shipping times, and much, much more.', 'wpsso' );
 
 				$notice_msg .= '<ul><li>' . implode( $glue = '</li><li>', $action_links ) . '</li></ul>' . ' ';
 
