f416e3e951d3167a4d25f170283be7254c0d7f07
JS Morisset <jsm@surniaulula.com>
2021-03-19 10:10:45 -0700

    v8.25.1

--- lib/messages.php
+++ lib/messages.php
@@ -2235,7 +2235,7 @@ if ( ! class_exists( 'WpssoMessages' ) ) {
 
 						 	$text = '<p class="status-msg">';
 
-							$text .= sprintf( __( 'oEmbed HTML is provided by the WordPress or theme <code>%s</code> template, which may not use all available oEmbed data.', 'wpsso' ), 'embed-content' );
+							$text .= sprintf( __( 'oEmbed HTML is created by the <code>%1$s</code> template.', 'wpsso' ), 'wpsso/embed-content' );
 
 							$text .= '</p>';
 
