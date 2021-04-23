14a2d636973d278ab2f68fa8b8ff5cb285c8ada4
JS Morisset <jsm@surniaulula.com>
2021-04-23 09:48:57 -0700

    v8.26.4-rc.2

--- lib/config.php
+++ lib/config.php
@@ -2413,9 +2413,9 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 				),
 				'document_title' => array(	// Webpage Document Title.
 					'wp_title'         => 'WordPress Title',
-					'og_title'         => 'Default Title',
-					'schema_title'     => 'Schema Name (Title)',
-					'schema_title_alt' => 'Schema Alternate Name',
+					'og_title'         => 'Document SSO Default Title',
+					'schema_title'     => 'Document SSO Schema Name (Title)',
+					'schema_title_alt' => 'Document SSO Schema Alternate Name',
 				),
 				'notice_systems' => array(
 					'toolbar_notices' => 'SSO Toolbar Notices',
