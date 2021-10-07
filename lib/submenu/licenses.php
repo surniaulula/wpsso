bda7c0d7ccf970424c86874e5a60468ef38262c5
JS Morisset <jsm@surniaulula.com>
2021-10-07 09:09:21 -0700

    v9.1.0

--- lib/submenu/licenses.php
+++ lib/submenu/licenses.php
@@ -34,6 +34,31 @@ if ( ! class_exists( 'WpssoSubmenuLicenses' ) && class_exists( 'WpssoAdmin' ) ) 
 		}
 
 		/**
+		 * Called by WpssoAdmin->load_setting_page() after the 'wpsso-action' query is handled.
+		 *
+		 * Add settings page filter and action hooks.
+		 */
+		protected function add_plugin_hooks() {
+
+			$this->p->util->add_plugin_filters( $this, array(
+				'form_button_rows' => 1,	// Filter form buttons for this settings page only.
+			) );
+		}
+
+		/**
+		 * Remove the "Change to View" button from the settings page.
+		 */
+		public function filter_form_button_rows( $form_button_rows ) {
+
+			if ( isset( $form_button_rows[ 0 ] ) ) {
+
+				$form_button_rows[ 0 ] = SucomUtil::preg_grep_keys( '/^change_show_options/', $form_button_rows[ 0 ], $invert = true );
+			}
+
+			return $form_button_rows;
+		}
+
+		/**
 		 * Called by the extended WpssoAdmin class.
 		 */
 		protected function add_meta_boxes() {
