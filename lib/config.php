6decdce67b6230be6d642d54c2ed09427e597845
JS Morisset <jsm@surniaulula.com>
2021-11-15 16:43:34 -0800

    v9.8.0

--- lib/config.php
+++ lib/config.php
@@ -1183,7 +1183,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 				'wpssowpsm' => array(			// Plugin acronym.
 					'short'       => 'WPSSO WPSM',	// Short plugin name.
 					'name'        => 'WPSSO WP Sitemaps',
-					'desc'        => 'Manage post and taxonomy types included in the WordPress sitemaps XML, and exclude content marked as "No Index".',
+					'desc'        => 'Manage post and taxonomy types included in the WordPress sitemaps XML and exclude content marked as "No Index".',
 					'slug'        => 'wpsso-wp-sitemaps',
 					'base'        => 'wpsso-wp-sitemaps/wpsso-wp-sitemaps.php',
 					'update_auth' => '',		// No premium version.
