7e18daa8f1df787c362529e58935976fd97c28a9
JS Morisset <jsm@surniaulula.com>
2021-11-20 10:01:45 -0800

    v9.8.1

--- wpsso.php
+++ wpsso.php
@@ -15,7 +15,7 @@
  * Requires At Least: 5.2
  * Tested Up To: 5.8.2
  * WC Tested Up To: 5.9.0
- * Version: 9.8.1-rc.1
+ * Version: 9.8.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
