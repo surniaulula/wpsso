62f5892b5e01cc2d9ab8e2392cfeb39da74ea347
JS Morisset <jsm@surniaulula.com>
2022-01-19 06:36:29 -0800

    v9.14.0-b.2

--- lib/abstract/wp-meta.php
+++ lib/abstract/wp-meta.php
@@ -1607,6 +1607,12 @@ if ( ! class_exists( 'WpssoAbstractWpMeta' ) ) {
 
 					unset( $md_opts[ $md_pre . '_img_url' ] );
 				}
+				
+				if ( empty( $md_opts[ $md_pre . '_img_url' ] ) ) {	// Just in case.
+
+					unset( $md_opts[ $md_pre . '_img_url:width' ] );
+					unset( $md_opts[ $md_pre . '_img_url:height' ] );
+				}
 			}
 
 			/**
