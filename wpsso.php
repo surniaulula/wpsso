7a8d3a422dd1186733d5959388cc4961849ed127
JS Morisset <jsm@surniaulula.com>
2022-02-09 16:30:36 -0800

    v10.3.0-b.1

--- wpsso.php
+++ wpsso.php
@@ -15,7 +15,7 @@
  * Requires At Least: 5.2
  * Tested Up To: 5.9.0
  * WC Tested Up To: 6.2.0
- * Version: 10.3.0-dev.6
+ * Version: 10.3.0-b.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
