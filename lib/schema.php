47bdaaf093efb37dec57c2fee92b200f39b07bdd
JS Morisset <jsm@surniaulula.com>
2022-02-28 13:55:57 -0800

    v11.3.1

--- lib/schema.php
+++ lib/schema.php
@@ -57,17 +57,17 @@ if ( ! class_exists( 'WpssoSchema' ) ) {
 
 			$sizes[ 'schema_1x1' ] = array(		// Option prefix.
 				'name'         => 'schema-1x1',
-				'label_transl' => _x( 'Schema 1:1 (Google)', 'option label', 'wpsso' ),
+				'label_transl' => _x( 'Schema 1:1 (Google Rich Results)', 'option label', 'wpsso' ),
 			);
 
 			$sizes[ 'schema_4x3' ] = array(		// Option prefix.
 				'name'         => 'schema-4x3',
-				'label_transl' => _x( 'Schema 4:3 (Google)', 'option label', 'wpsso' ),
+				'label_transl' => _x( 'Schema 4:3 (Google Rich Results)', 'option label', 'wpsso' ),
 			);
 
 			$sizes[ 'schema_16x9' ] = array(	// Option prefix.
 				'name'         => 'schema-16x9',
-				'label_transl' => _x( 'Schema 16:9 (Google)', 'option label', 'wpsso' ),
+				'label_transl' => _x( 'Schema 16:9 (Google Rich Results)', 'option label', 'wpsso' ),
 			);
 
 			$sizes[ 'thumb' ] = array(		// Option prefix.
