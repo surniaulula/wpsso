bcff0ed5f457d9902f5e88e755b6bd4d2768bdad
JS Morisset <jsm@surniaulula.com>
2022-03-01 13:48:56 -0800

    v11.4.0-dev.2

--- wpsso.php
+++ wpsso.php
@@ -19,7 +19,7 @@
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
- *      {major}         Major structural code changes / re-writes or incompatible API changes.
+ *      {major}         Major structural code changes and/or incompatible API changes (ie. risk of breaking changes).
  *      {minor}         New functionality was added or improved in a backwards-compatible manner.
  *      {bugfix}        Backwards-compatible bug fixes or small improvements.
  *      {stage}.{level} Pre-production release: dev < a (alpha) < b (beta) < rc (release candidate).
