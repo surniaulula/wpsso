a0392cbe8daf01d1125949b277da286f9b611c06
JS Morisset <jsm@surniaulula.com>
2022-03-10 06:52:41 -0800

    v11.6.0-rc.1

--- wpsso.php
+++ wpsso.php
@@ -15,7 +15,7 @@
  * Requires At Least: 5.2
  * Tested Up To: 5.9.1
  * WC Tested Up To: 6.3.0
- * Version: 11.6.0-b.1
+ * Version: 11.6.0-rc.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
