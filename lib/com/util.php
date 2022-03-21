3aca344e701926737e91613c9ad02e1a6e6d9161
JS Morisset <jsm@surniaulula.com>
2022-03-21 13:43:12 -0700

    v11.7.2-dev.5

--- lib/com/util.php
+++ lib/com/util.php
@@ -4413,11 +4413,6 @@ if ( ! class_exists( 'SucomUtil' ) ) {
 
 		public static function get_json_scripts( $html, $do_decode = true ) {
 
-			if ( function_exists( 'mb_convert_encoding' ) ) {	// Just in case.
-
-				$html = mb_convert_encoding( $html, $to_encoding = 'HTML-ENTITIES', $from_encoding = 'UTF-8' );
-			}
-
 			/**
 			 * Remove containers that should not include json scripts.
 			 *
