87c874bec8dc27cf47bd0fd55b4adb52e42a271b
JS Morisset <jsm@surniaulula.com>
2022-03-22 10:34:19 -0700

    v11.7.2-b.3

--- lib/config.php
+++ lib/config.php
@@ -2125,7 +2125,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			 * Update manager config.
 			 */
 			'um' => array(
-				'rec_version' => '4.12.2',	// Minimum update manager version (soft limit).
+				'rec_version' => '4.12.3-b.3',	// Minimum update manager version (soft limit).
 				'check_hours' => array(
 					24  => 'Every day',
 					48  => 'Every two days',
