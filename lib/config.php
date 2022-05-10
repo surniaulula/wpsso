5da694af2702a640f9bd2f9d7c8328d7c962b84a
JS Morisset <jsm@surniaulula.com>
2022-05-10 06:01:21 -0700

    v12.2.2

--- lib/config.php
+++ lib/config.php
@@ -21,7 +21,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			),
 			'plugin' => array(
 				'wpsso' => array(			// Plugin acronym.
-					'version'     => '12.2.1',	// Plugin version.
+					'version'     => '12.2.2',	// Plugin version.
 					'opt_version' => '903',		// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core',
--- wpsso.php
+++ wpsso.php
@@ -15,7 +15,7 @@
  * Requires At Least: 5.2
  * Tested Up To: 6.0.0
  * WC Tested Up To: 6.4.1
- * Version: 12.2.1
+ * Version: 12.2.2
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
