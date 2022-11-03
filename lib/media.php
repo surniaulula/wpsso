21389e44b8ee32073e6351c476b3eda6bb81e622
JS Morisset <jsm@surniaulula.com>
2022-11-03 06:17:53 -0700

    v13.7.1-dev.13

--- lib/media.php
+++ lib/media.php
@@ -138,7 +138,9 @@ if ( ! class_exists( 'WpssoMedia' ) ) {
 				 * If there are other image sizes using the same dimensions, then copy the image and leave the
 				 * original for those image sizes.
 				 */
-				if ( $this->p->util->count_other_sizes( $img_info[ 'size_name' ], $img_info[ 'pid' ] ) > 0 ) {
+				$identical_sizes = $this->p->util->count_identical_sizes( $img_info[ 'size_name' ], $img_info[ 'pid' ] );
+
+				if ( $identical_sizes > 0 ) {
 
 					if ( copy( $file_path, $new_file_path ) ) {	// Another plugin/theme uses the same dimensions.
 
--- lib/util.php
+++ lib/util.php
@@ -382,6 +382,34 @@ if ( ! class_exists( 'WpssoUtil' ) ) {
 		}
 
 		/**
+		 * Since Wpsso Core v12.3.0.
+		 */
+		public function count_identical_sizes( $size_name, $attachment_id = false ) {
+
+			$size_info   = $this->get_size_info( $size_name, $attachment_id );	// Uses a local static cache.
+			$image_sizes = $this->get_image_sizes( $attachment_id );
+			$count       = 0;
+
+			foreach ( $image_sizes as $key => $arr ) {
+
+				if ( 0 !== strpos( $key, 'wpsso-' ) ) {
+
+					/**
+					 * Use '==' to allow comparing strings to integers.
+					 */
+					if ( $arr[ 'width' ] == $size_info[ 'width' ] &&
+						$arr[ 'height' ] == $size_info[ 'height' ] &&
+							$arr[ 'is_cropped' ] == $size_info[ 'is_cropped' ] ) {
+
+						$count++;
+					}
+				}
+			}
+
+			return $count;
+		}
+
+		/**
 		 * Get the width, height and crop value for all image sizes.
 		 *
 		 * Returns an associative array with the image size name as the array key.
@@ -396,36 +424,6 @@ if ( ! class_exists( 'WpssoUtil' ) ) {
 			}
 
 			return $image_sizes;
-		}
-
-		/**
-		 * Since Wpsso Core v12.3.0.
-		 */
-		public function count_other_sizes( $size_name, $attachment_id = false ) {
-
-			$size_info = $this->get_size_info( $size_name, $attachment_id );	// Uses a local static cache.
-
-			$image_sizes = $this->get_image_sizes( $attachment_id );
-
-			$count = 0;
-
-			foreach ( $image_sizes as $key => $arr ) {
-
-				if ( 0 !== strpos( $key, 'wpsso-' ) ) {
-
-					/**
-					 * Allow comparing strings to integers.
-					 */
-					if ( $arr[ 'width' ] == $size_info[ 'width' ] &&
-						$arr[ 'height' ] == $size_info[ 'height' ] &&
-							$arr[ 'is_cropped' ] == $size_info[ 'is_cropped' ] ) {
-
-						$count++;
-					}
-				}
-			}
-
-			return $count;
 		}
 
 		/**
