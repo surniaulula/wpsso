edbde2d67f8fbb1d9ac3cd1be538d236aefb4f09
JS Morisset <jsm@surniaulula.com>
2023-01-26 09:13:49 -0800

    v14.7.0

--- lib/head.php
+++ lib/head.php
@@ -791,7 +791,7 @@ if ( ! class_exists( 'WpssoHead' ) ) {
 					$home_url   = SucomUtilWP::raw_get_home_url();
 					$home_path  = preg_replace( '/^[a-z]+:\/\//i', '', $home_url );	// Remove the protocol prefix.
 
-					return '<meta name="wpsso-' . $type . '" content="' . date( 'c' ) . ' in ' . $total_secs .  ' for ' . $home_path . '">' . "\n";
+					return '<meta name="wpsso-' . $type . '" content="' . date( 'c' ) . ' in ' . $total_secs .  ' for ' . $home_path . '"/>' . "\n";
 
 				case 'begin':
 				case 'end':
@@ -800,7 +800,7 @@ if ( ! class_exists( 'WpssoHead' ) ) {
 
 				case 'cached':
 
-					return '<meta name="wpsso-' . $type . '" content="' . ( $args ? date( 'c' ) : 'no cache' ) . '">' . "\n";
+					return '<meta name="wpsso-' . $type . '" content="' . ( $args ? date( 'c' ) : 'no cache' ) . '"/>' . "\n";
 
 				/*
 				 * Used by WpssoPost->check_post_head() and WpssoSsmFilters->strip_schema_microdata().
