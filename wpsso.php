94524f14bada01292f761359d3d420dfa92141b7
JS Morisset <jsm@surniaulula.com>
2023-02-16 15:12:53 -0800

    v15.3.1-rc.1

--- wpsso.php
+++ wpsso.php
@@ -15,7 +15,7 @@
  * Requires At Least: 5.4
  * Tested Up To: 6.1.1
  * WC Tested Up To: 7.4.0
- * Version: 15.3.1-b.1
+ * Version: 15.3.1-rc.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
