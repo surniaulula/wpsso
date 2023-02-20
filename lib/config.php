0d4891ac8d456a4e5303cedbef11749cf48a98b3
JS Morisset <jsm@surniaulula.com>
2023-02-20 11:00:13 -0800

    v15.4.0

--- lib/config.php
+++ lib/config.php
@@ -21,7 +21,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			),
 			'plugin' => array(
 				'wpsso' => array(			// Plugin acronym.
-					'version'     => '15.4.0-rc.1',	// Plugin version.
+					'version'     => '15.4.0',	// Plugin version.
 					'opt_version' => '955',		// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core',
@@ -2353,7 +2353,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			 * Update manager config.
 			 */
 			'um' => array(
-				'rec_version' => '4.16.0-rc.1',	// Minimum update manager version (soft limit).
+				'rec_version' => '4.16.0',	// Minimum update manager version (soft limit).
 				'check_hours' => array(
 					24  => 'Every day',
 					48  => 'Every two days',
