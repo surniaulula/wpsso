12eb77904ea9baf9713ad98ec718f6d5cefa638f
JS Morisset <jsm@surniaulula.com>
2023-02-23 06:41:33 -0800

    v15.4.1-rc.1

--- lib/script.php
+++ lib/script.php
@@ -365,7 +365,7 @@ EOF;
 			$admin_l10n = $this->p->cf[ 'plugin' ][ 'wpsso' ][ 'admin_l10n' ];
 
 			wp_register_script( 'sucom-admin-page', WPSSO_URLPATH . 'js/com/jquery-admin-page.' . $this->file_ext,
-				$deps = array( 'jquery' ), '20220318-02', $in_footer = true );
+				$deps = array( 'jquery' ), $this->version, $in_footer = true );
 
 			wp_localize_script( 'sucom-admin-page', $admin_l10n, $this->get_admin_page_script_data() );
 		}
