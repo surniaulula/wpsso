19a8ba5f8d913fb315cc62a490e82b23aa9d80c2
JS Morisset <jsm@surniaulula.com>
2023-08-02 02:34:51 -0700

    v15.18.2-dev.1

--- lib/admin-head-suggest-options.php
+++ lib/admin-head-suggest-options.php
@@ -83,7 +83,7 @@ if ( ! class_exists( 'WpssoAdminHeadSuggestOptions' ) ) {
 					$notice_key = 'notice-wc-inherit-featured-disabled';
 
 					if ( $this->p->notice->is_admin_pre_notices( $notice_key ) ) {
-		
+
 						if ( $notice_msg = $this->p->msgs->get( $notice_key ) ) {
 
 							$this->p->notice->inf( $notice_msg, null, $notice_key, $dismiss_time = true );
