46da11f676cc6e620f6168e1242e22873dc4af35
JS Morisset <jsm@surniaulula.com>
2023-08-05 08:02:20 -0700

    v15.18.2

--- lib/config.php
+++ lib/config.php
@@ -2540,8 +2540,8 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			 */
 			'wp' => array(
 				'label'       => 'WordPress',
-				'min_version' => '5.4',		// Hard limit - deactivate the plugin when activating.
-				'rec_version' => '6.1.1',	// Soft limit - issue warning if lower version found.
+				'min_version' => '5.5',		// Hard limit - deactivate the plugin when activating.
+				'rec_version' => '6.2.2',	// Soft limit - issue warning if lower version found.
 				'version_url' => 'https://codex.wordpress.org/Supported_Versions?nocache=1',
 				'tb_iframe'   => array(		// Thickbox iframe.
 					'width'  => 772,	// Url query argument.
