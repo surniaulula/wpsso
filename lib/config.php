1a5c11173a5c6a2516cae14312ea482c530e5d17
JS Morisset <jsm@surniaulula.com>
2023-09-30 04:00:14 -0700

    v16.1.0

--- lib/config.php
+++ lib/config.php
@@ -21,7 +21,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			),
 			'plugin' => array(
 				'wpsso' => array(			// Plugin acronym.
-					'version'     => '16.1.0-rc.2',	// Plugin version.
+					'version'     => '16.1.0',	// Plugin version.
 					'opt_version' => '991',		// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core',
