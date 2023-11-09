403f96bedcb0ce9b301ac1dc22c35ca0960ef1e7
JS Morisset <jsm@surniaulula.com>
2023-11-08 18:31:10 -0800

    v17.1.0-dev.1

--- wpsso.php
+++ wpsso.php
@@ -13,7 +13,7 @@
  * Description: Present your content at its best on social sites and in search results - no matter how URLs are shared, reshared, messaged, posted, embedded, or crawled.
  * Requires PHP: 7.2.34
  * Requires At Least: 5.5
- * Tested Up To: 6.4.0
+ * Tested Up To: 6.4.1
  * WC Tested Up To: 8.2.1
  * Version: 17.1.0-dev.1
  *
