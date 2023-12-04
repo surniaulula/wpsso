8f27329a7390807bb38ae866b754481bcf026fd0
JS Morisset <jsm@surniaulula.com>
2023-12-04 07:31:08 -0800

    v17.3.1-dev.3

--- lib/config.php
+++ lib/config.php
@@ -22,7 +22,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			'plugin' => array(
 				'wpsso' => array(			// Plugin acronym.
 					'version'     => '17.3.1-dev.3',	// Plugin version.
-					'opt_version' => '997',		// Increment when changing default option values.
+					'opt_version' => '998',		// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core',
 					'desc'        => 'Present your content at its best on social sites and in search results - no matter how URLs are shared, reshared, messaged, posted, embedded, or crawled.',
