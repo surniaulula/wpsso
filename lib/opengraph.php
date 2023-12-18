022d427fe51cb08ae0979688e543093efa0edda1
JS Morisset <jsm@surniaulula.com>
2023-12-18 05:29:23 -0800

    v17.5.0-dev.4

--- lib/opengraph.php
+++ lib/opengraph.php
@@ -732,93 +732,10 @@ if ( ! class_exists( 'WpssoOpenGraph' ) ) {
 
 			if ( $this->p->debug->enabled ) {
 
-				$this->p->debug->mark_diff( 'mt_og created' );
-				
 				$this->p->debug->log_arr( 'mt_og', $mt_og );
 			}
 
-			self::optimize_array( $mt_og );
-
-			if ( $this->p->debug->enabled ) {
-				
-				$this->p->debug->mark_diff( 'mt_og optimized' );
-			}
-
 			return $mt_og;
-		}
-
-		public static function optimize_array( array &$mt_og ) {	// Pass by reference is OK.
-			
-			$wpsso =& Wpsso::get_instance();
-
-			if ( $wpsso->debug->enabled ) {
-
-				$wpsso->debug->mark();
-			}
-
-			foreach ( $mt_og as $key => $value ) {
-					
-				$mt_og[ $key ] =& self::optimize_array_cache( $value, $mt_og );
-			}
-		}
-
-		private static function &optimize_array_cache( $value, array &$mt_og ) {	// Pass by reference is OK.
-
-			$wpsso =& Wpsso::get_instance();
-
-			static $local_recursion = 0;
-			
-			if ( $local_recursion > 32 ) {	// Just in case.
-
-				return $value;
-
-			} elseif ( is_array( $value ) ) {
-
-				foreach ( $value as $recurs_key => $recurs_val ) {
-
-					$local_recursion++;
-
-					$value[ $recurs_key ] =& self::optimize_array_cache( $recurs_val, $mt_og );
-			
-					$local_recursion--;
-				}
-			}
-
-			/*
-			 * The associative array key can be a string or integer. The value can be any type.
-			 *
-			 * Strings containing valid decimal ints, unless the number is preceded by a + sign, will be cast to the
-			 * int type. E.g. the key "8" will actually be stored under 8. On the other hand "08" will not be cast, as
-			 * it isn't a valid decimal integer.
-			 *
-			 * Floats are also cast to ints, which means that the fractional part will be truncated. E.g. the key 8.7
-			 * will actually be stored under 8.
-			 *
-			 * Bools are cast to ints, too, i.e. the key true will actually be stored under 1 and the key false under
-			 * 0.
-			 *
-			 * Null will be cast to the empty string, i.e. the key null will actually be stored under "".
-			 *
-			 * Arrays and objects can not be used as keys. Doing so will result in a warning: Illegal offset type.
-			 *
-			 * See https://www.php.net/manual/en/language.types.array.php
-			 */
-			if ( is_string( $value ) ) {
-
-				$key = strlen( $value ) > 32 || is_numeric( $value ) ? md5( $value ) : $value;
-
-			} elseif ( is_int( $value ) ) {
-
-				$key = $value;
-
-			} else $key = md5( serialize( $value ) );
-
-			if ( ! isset( $mt_og[ '__cache' ][ $key ] ) ) {
-
-				$mt_og[ '__cache' ][ $key ] = $value;
-			}
-
-			return $mt_og[ '__cache' ][ $key ];
 		}
 
 		public function get_og_type_id_for( $opt_suffix, $default_id = null ) {
