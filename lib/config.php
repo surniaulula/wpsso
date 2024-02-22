1a1191ec770c3f89cb783e8ee4ce1bcee3e13f2c
jsmoriss <jsm@surniaulula.com>
2024-02-22 15:19:13 +0000

    v17.14.1-rc.1

--- lib/config.php
+++ lib/config.php
@@ -21,7 +21,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			),
 			'plugin' => array(
 				'wpsso' => array(			// Plugin acronym.
-					'version'     => '17.14.1-dev.1',	// Plugin version.
+					'version'     => '17.14.1-rc.1',	// Plugin version.
 					'opt_version' => '999',		// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core',
