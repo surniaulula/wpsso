c580a99b89ea512bbf8054592e9666caaf44543c
jsmoriss <jsm@surniaulula.com>
2024-02-22 21:08:14 +0000

    v17.14.2

--- lib/config.php
+++ lib/config.php
@@ -25,7 +25,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 					'opt_version' => '999',		// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core',
-					'desc'        => 'Present your content at its best on social sites and in search results - no matter how URLs are shared, reshared, messaged, posted, embedded, or crawled.',
+					'desc'        => 'Present your content at its best on social sites and search results - no matter how URLs are shared, reshared, messaged, posted, embedded, or crawled.',
 					'slug'        => 'wpsso',
 					'base'        => 'wpsso/wpsso.php',
 					'update_auth' => 'tid',
