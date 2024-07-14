52a764689bbbb812d7028bba30ab062e76d6c44f
jsmoriss <jsm@surniaulula.com>
2024-07-14 23:50:29 +0000

    v17.19.0-dev.1

--- lib/functions.php
+++ lib/functions.php
@@ -32,11 +32,6 @@ if ( ! function_exists( '__return_ignore' ) ) {		// Useful for the 'wpsso_option
 	function __return_ignore() { return 'ignore'; }
 }
 
-if ( ! function_exists( '__return_ok_blank' ) ) {	// Useful for the 'wpsso_option_type_{base_key}' filter.
-
-	function __return_ok_blank() { return 'ok_blank'; }
-}
-
 if ( ! function_exists( '__return_hour_in_seconds' ) ) {
 
 	function __return_hour_in_seconds() { return HOUR_IN_SECONDS; }
