3511462539dfaaae8ddc50d15dc23f223538ab99
jsmoriss <jsm@surniaulula.com>
2024-08-20 23:10:51 +0000

    v18.3.0

--- lib/config.php
+++ lib/config.php
@@ -2546,7 +2546,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			 */
 			'um' => array(
 				'min_version' => '5.0.0',	// Released on 2023/11/08.
-				'rec_version' => '5.6.0',	// Released on 2024/08/20.
+				'rec_version' => '5.6.1',	// Released on 2024/08/20.
 				'version_filter' => array(
 					'dev'    => 'Development and Up',
 					'alpha'  => 'Alpha and Up',
