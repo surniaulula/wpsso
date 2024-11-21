dcddd7b64e0c40386ad41931084df4f970bd4119
jsmoriss <jsm@surniaulula.com>
2024-11-21 00:14:52 +0000

    v18.18.1

--- wpsso.php
+++ wpsso.php
@@ -15,7 +15,7 @@
  * Requires At Least: 5.9
  * Tested Up To: 6.7.1
  * WC Tested Up To: 9.4.1
- * Version: 18.18.1-rc.1
+ * Version: 18.18.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
