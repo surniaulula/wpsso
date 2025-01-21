9bab08fe73a8643925e134f5adc00421f6d1571d
jsmoriss <jsm@surniaulula.com>
2025-01-21 17:06:42 +0000

    v18.18.5-dev.2

--- lib/config.php
+++ lib/config.php
@@ -21,7 +21,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			),
 			'plugin' => array(
 				'wpsso' => array(			// Plugin acronym.
-					'version'     => '18.18.5-dev.1',	// Plugin version.
+					'version'     => '18.18.5-dev.2',	// Plugin version.
 					'opt_version' => '1021',	// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core',
