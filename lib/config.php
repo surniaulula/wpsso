35fa51119204fd88dc58ff850b31fe6494e36448
jsmoriss <jsm@surniaulula.com>
2025-06-15 01:00:48 +0000

    v20.0.0

--- lib/config.php
+++ lib/config.php
@@ -21,7 +21,7 @@ if ( ! class_exists( 'WpssoConfig' ) ) {
 			),
 			'plugin' => array(
 				'wpsso' => array(			// Plugin acronym.
-					'version'     => '20.0.0-rc.1',	// Plugin version.
+					'version'     => '20.0.0',	// Plugin version.
 					'opt_version' => '1026',	// Increment when changing default option values.
 					'short'       => 'WPSSO Core',	// Short plugin name.
 					'name'        => 'WPSSO Core',
