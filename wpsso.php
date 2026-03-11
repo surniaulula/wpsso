9b8e099751eb6274b94b0f3eb86549b5e35b7878
jsmoriss <jsm@surniaulula.com>
2026-03-11 18:16:39 +0000

    v21.11.2

--- wpsso.php
+++ wpsso.php
@@ -13,7 +13,7 @@
  * Description: Present your content at its best for social sites and search results, no matter how URLs are shared, reshared, messaged, posted, embedded, or crawled.
  * Requires PHP: 7.4.33
  * Requires At Least: 6.0
- * Tested Up To: 6.9.2
+ * Tested Up To: 6.9.4
  * WC Tested Up To: 10.6.0
  * Version: 21.11.2
  *
