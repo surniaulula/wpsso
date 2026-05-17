45885b3b170f5b27d5b3713b4d3509425f4ac012
jsmoriss <jsm@surniaulula.com>
2026-05-17 00:53:22 +0000

    v22.0.0

--- wpsso.php
+++ wpsso.php
@@ -14,7 +14,7 @@
  * Requires PHP: 7.4.33
  * Requires At Least: 6.0
  * Tested Up To: 6.9.4
- * WC Tested Up To: 10.6.2
+ * WC Tested Up To: 10.7.0
  * Version: 22.0.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
